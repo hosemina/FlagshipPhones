1be5586d03a2718332ad062c4ea87f8fa94c35a2
Emina Smajlovic <esmajlovic1@etf.unsa.ba>
2016-12-30 14:52:25 +0100

    Prepravke

--- FPPhoneSpecsForma.php
+++ FPPhoneSpecsForma.php
@@ -64,29 +64,7 @@ echo('<form enctype="multipart/form-data" method="post" action="dodajSpecifikaci
 </form>
 
 
-<?php
-include_once("fonoapi-master/fonoapi-v1.php");
 
-    $apiKey = "c4f79aace6d97ca8d4ba7ea8ab715085fc86abdf8cf72509"; // get your token key here - https://fonoapi.freshpixl.com
-    $fonoapi = fonoApi::init($apiKey);
-
-        try {
-
-            $res = $fonoapi::getDevice("S7"); // the device you need to get details here
-
-            foreach ($res as $mobile) {
-                if (!empty($mobile->DeviceName))    echo "Device : ". $mobile->DeviceName . "<br>";
-                if (!empty($mobile->Brand))         echo "Brand : ". $mobile->Brand . "<br>";
-                if (!empty($mobile->cpu))           echo "Cpu : " . $mobile->cpu . "<br>";
-                if (!empty($mobile->status))        echo "Status : " . $mobile->status . "<br>";
-                if (!empty($mobile->dimensions))    echo "Dimensions : " . $mobile->dimensions . "<br>";
-                if (!empty($mobile->_4g_bands))     echo "4g : " .$mobile->_4g_bands . "<br>";
-            }
-
-        } catch (Exception $e) {
-            echo "ERROR : " . $e->getMessage();
-        }
-        ?>
 </div></article>
 <?php
 include 'desno.php';
